b83bd849994bb00e819e92de66a4de0dc7bbd2f7
tranvuduchoang <81789536+tranvuduchoang@users.noreply.github.com>
2021-05-10 17:15:08 +0700

    updated itemsView.js
    beside updated itemsView.js , i also put new updates of itemsView.css and creat a new file: items.js

---
+++ src/Page/items.js
@@ -0,0 +1,61 @@
+const sizes=document.querySelectorAll('.size');
+const colors = document.querySelectorAll('.color');
+const shoes = document.querySelectorAll('.shoe');
+const bgcolors = document.querySelectorAll('.bgcolor');
+const shoebgs = document.querySelectorAll('.shoebg');
+
+let prevColor="black";
+let animationEnd=true;
+
+function changeSize(){
+    sizes.forEach(size=>size.classList.remove('active'));
+    this.classList.add('active');
+}
+
+function changeColor(){
+    if(!animationEnd)return;
+    let primary=this.getAttribute('primary');
+    let color=this.getAttribute('color');
+    let shoe=document.querySelector(`.shoe[color="${color}"]`);
+    let bgcolor=document.querySelector(`.bgcolor[color="${color}"]`);
+    let prevBgcolor = document.querySelector(`.bgcolor[color="${prevColor}"]`);
+
+    if(color==prevColor) return;
+
+    colors.forEach(c=>c.classList.remove('active'));
+    this.classList.add('active');
+
+    document.documentElement.style.setProperty('--primary',primary);
+
+    shoes.forEach(s=>s.classList.remove('show'));
+    shoe.classList.add('show');
+
+    bgcolors.forEach(g => g.classList.remove('first','second'));
+    bgcolor.classList.add('first');
+    prevBgcolor.classList.add('second');
+
+    prevBgcolor=color;
+    animationEnd=false;
+
+    bgcolor.addEventListener('animationend',()=>{
+        animationEnd=true;
+    })
+}
+
+sizes.forEach(size=>size.addEventListener('click',changeSize));
+colors.forEach(c=>c.addEventListener('click',changeColor));
+
+let x=window.matchMedia("(max-width:1000px)");
+
+function changeHeight(){
+    if(x.matches){
+        let shoeHeight=shoes[0].offsetHeight;
+        shoebgs.style.height=`${shoeHeight*0.9}px`;
+    }
+    else{
+        shoebgs.style.height="475px";
+    }
+}
+changeHeight();
+
+window.addEventListener('resize',changeHeight);
--- src/Page/itemsView.js
+++ src/Page/itemsView.js
@@ -6,68 +6,75 @@ class ItemView extends React.Component {
     render() {
         return (
             <div>
-                <div className="section-title text-center">
+                <div class="section-title text-center">
                     <h1>welcome here</h1>
                 </div>
-                <div className="container">
-                    <div className="row">
-                        <div className="content-info">
-                            <div className="shoebrand-name">
+                <div class="container">
+                    <div class="row">
+                        <div class="content-info">
+                            <div class="shoebrand-name">
                                 <div>
-                                    <h1 className="brand">NIKE SHOSE</h1>
+                                    <h1 class="brand">NIKE SHOSE</h1>
                                 </div>
-                                <h3 className="small">Men's SPORT Shose</h3>
+                                <h3 class="small">Men's SPORT Shose</h3>
                             </div>
-                            <div className="content-detail">
-                                <h3 className="title">Product Information</h3>
-                                <p className="content-in">The Nike Edge 270 take the look of retro basketball and puts it through a modern lens</p>
+                            <div class="content-detail">
+                                <h3 class="title">Product Information</h3>
+                                <p class="content-in">The Nike Edge 270 take the look of retro basketball and puts it through a
+                        modern lenssssssssssssssssssssssssssssssssssssssssssssssssss</p>
                             </div>
-                            <div className="colors-fill">
-                                <h3 className="title">Colors</h3>
-                                <div className="Colors">
-                                    <span className="color active" primary="#444" color="black"></span>
-                                    <span className="color" primary="#29b864" color="grean"></span>
-                                    <span className="color" primary="#ff5521" color="orange"></span>
-                                    <span className="color" primary="#2175f5" color="blue"></span>
-                                    <span className="color" primary="#f84848" color="red"></span>
-                                </div>
-                            </div>
-                            <div className="size-ofproduct">
-                                <h3 className="title">Size</h3>
-                                <div className="Size">
-                                    <span className="size active">8</span>
-                                    <span className="size">8.5</span>
-                                    <span className="size">9</span>
-                                    <span className="size">8.5</span>
-                                    <span className="size">10</span>
-                                    <span className="size">10.5</span>
-                                    <span className="size">11</span>
-                                    <span className="size">11.5</span>
-                                    <span className="size">12</span>
-                                    <span className="size">12.5</span>
+                            <div class="colors-fill">
+                                <h3 class="title">Colors</h3>
+                                <div class="Colors">
+                                    <span class="color active" primary="#444" color="black"></span>
+                                    <span class="color" primary="#29b864" color="green"></span>
+                                    <span class="color" primary="#ff5521" color="orange"></span>
+                                    <span class="color" primary="#2175f5" color="blue"></span>
+                                    <span class="color" primary="#f84848" color="red"></span>
                                 </div>
                             </div>
-                            <div className="price-ofshoe">
-                                <a href="" className="price-of"><i className="fas fa-shopping-cart"></i>Add to cart</a>
-                                <div className="share">
+                            <div class="size-ofproduct">
+                                <h3 class="title">Size</h3>
+                                <div class="sizes">
+                                    <span class="size active">8</span>
+                                    <span class="size">8.5</span>
+                                    <span class="size">9</span>
+                                    <span class="size">8.5</span>
+                                    <span class="size">10</span>
+                                    <span class="size">10.5</span>
+                                    <span class="size">11</span>
+                                    <span class="size">11.5</span>
+                                    <span class="size">12</span>
+                                    <span class="size">12.5</span>
+                                </div>
+                            </div>
+                            <div class="price-shoe">
+                                <a href="" class="price-of"><i class="fas fa-shopping-cart"></i>Add to cart</a>
+                                <div class="share">
                                     <span>share</span>
                                 </div>
-                                <div className="price">
+                                <div class="price">
                                     <h1>&dollar;95.97</h1>
                                 </div>
                             </div>
                         </div>
-                        <div className="container-cart">
-                            <div className="shoe-bg">
-                                <div className="bg-colors">
-                                    <div className="bg-color" color="black"></div>
-                                    <div className="bg-color" color="grean"></div>
-                                    <div className="bg-color" color="orange"></div>
-                                    <div className="bg-color" color="blue"></div>
-                                    <div className="bg-color" color="red"></div>
-                                </div>
-
+                    </div>
+                    <div class="container-cart">
+                        <div class="shoebg">
+                            <div class="bgcolors">
+                                <div class="bgcolor second" color="black"></div>
+                                <div class="bgcolor" color="grean"></div>
+                                <div class="bgcolor" color="orange"></div>
+                                <div class="bgcolor" color="blue"></div>
+                                <div class="bgcolor" color="red"></div>
                             </div>
+
+                            <img id="featured" src="./images/black.png" alt="" class="shoe showing" color="black"></img>
+                            <img src="./images/green.png" alt="" class="shoe showing" color="green"></img>
+                            <img src="./images/orange.png" alt="" class="shoe showing" color="orange"></img>
+                            <img src="./images/blue.png" alt="" class="shoe showing" color="blue"></img>
+                            <img src="./images/red.png" alt="" class="shoe showing" color="red"></img>
+
                         </div>
                     </div>
                 </div>
@@ -75,5 +82,5 @@ class ItemView extends React.Component {
         )
     }
 }
-
+<script src="./product.js"></script>
 export default ItemView;
