a8a3975f69c36ddfdee5739ec167219c443ee56f
SunFoxY2K <phuc1012000@gmail.com>
2021-05-13 14:41:34 +0700

    Footer without CSS

---
+++ src/Component/Footer/Footer.js
@@ -0,0 +1,87 @@
+import "./Footer.less";
+import React from 'react';
+import Footer from 'rc-footer';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+
+
+export default function FooterComponent() {
+    return (
+        <div>
+            <Footer
+                // theme={"light"}
+                // columnLayout={"space-between"}
+                columns={[
+                    {
+                        title: 'Learn More',
+                        items: [
+                            {
+                                title: 'About Us',
+                                url: '#',
+                            },
+                            {
+                                title: 'Privacy Policy',
+                                url: '#',
+                            },
+                            {
+                                title: 'Jobs',
+                                url: '#',
+                            },
+                        ]
+                    },
+                    {
+                        title: 'Products Brands',
+                        items: [
+                            {
+                                title: 'Gucci',
+                                url: 'https://www.gucci.com',
+                                openExternal: true,
+                            },
+                            {
+                                title: 'Nike',
+                                url: 'https://www.nike.com',
+                                openExternal: true,
+                            },
+                            {
+                                title: 'Puma',
+                                url: 'https://us.puma.com',
+                                openExternal: true,
+                            }
+                        ]
+                    },
+                    {
+                        title: 'Contact Us',
+                        items: [
+                            {
+                                title: 'Phone Number : +84 123456789',
+                            },
+                            {
+                                title: 'Email : MeoMeo@gmail.com',
+                            }
+                        ]
+                    },
+                    {
+                        title: 'Social',
+                        items:
+                            [
+                                {
+                                    title: "Facebook Page",
+                                    LinkComponent: FacebookIcon,
+                                    url: "https://www.facebook.com",
+                                    openExternal: true,
+                                },
+                                {
+                                    title: "Instagram Page",
+                                    LinkComponent: InstagramIcon,
+                                    url: "https://www.instagram.com/",
+                                    openExternal: true,
+                                }
+                            ]
+                    }
+                ]}
+                bottom="copyright © A_TEAM | All Right Reserved"
+            />
+        </div>
+    );
+}
+
