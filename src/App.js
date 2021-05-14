995b79ece4c3ace7f5f35a5c5c30605db93e9c96
NQPhuc <11730168+NQPhuc@users.noreply.github.com>
2021-05-14 20:52:20 +0700

    Fix Tphuc conflict, disable paymentpopup

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import reactDom from 'react-dom';
 import AddItem from './Component/AddItem.js';
 import ItemView from './Page/itemsView.js';
 import CartView from './Page/CartView.js';
-import PaymentPopup from "./Component/Payment/Payment";
+//import PaymentPopup from "./Component/Payment/Payment";
 
 //const backendAddress = "http://localhost:3030";
 
@@ -73,7 +73,7 @@ function App() {
             </Route>
           </Switch>
         <FooterComponent />
-        <PaymentPopup />
+        {/*<PaymentPopup />*/}
       </Router>
     </div>
   );
